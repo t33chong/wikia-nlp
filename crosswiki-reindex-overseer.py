9df3ab0cffd61a619091027d889ba65e723d6e9f
Tristan Euan Chong <tristaneuan@gmail.com>
2013-08-21 23:46:02 +0000

    removed superfluous files

--- crosswiki-reindex-overseer.py
+++
@@ -1,6 +0,0 @@
-from WikiaSolr import CrossWikiReindexOverseer, get_config
-import sys
-
-offset = 0 if len(sys.argv) == 2 else sys.argv[2]
-
-CrossWikiReindexOverseer(get_config(), {'workers':sys.argv[1], 'offset':offset}).oversee()
