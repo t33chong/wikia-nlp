a6b18c5379ad25a73c3db6f3d0e00429f85335c0
Tristan Euan Chong <tristaneuan@gmail.com>
2013-08-23 22:42:00 +0000

    changed default worker and thread values

--- nlp-overseer.py
+++ nlp-overseer.py
@@ -9,7 +9,7 @@ from optparse import OptionParser
 from WikiaSolr import NLPOverseer
 
 parser = OptionParser()
-parser.add_option("-n", "--workers", dest="workers", action="store", default=10,
+parser.add_option("-n", "--workers", dest="workers", action="store", default=4,
                   help="Specifies the number of open worker processes")
 parser.add_option("-w", "--wam-threshold", dest="wam_threshold", action="store", default=None,
                   help="If we have a threshold, we restart our wiki iteration when we hit it")
@@ -17,7 +17,7 @@ parser.add_option("-f", "--filter-query", dest="filterQuery", action="store", de
                   help="Filter queries can be used to slice and dice what we're iterating over")
 parser.add_option("-s", "--sort", dest="sort", action="store", default="wam desc",
                   help="Sorting groups lets us prioritize which wikis we act on first")
-parser.add_option("-t", "--threads", dest="threads", action="store", default="4",
+parser.add_option("-t", "--threads", dest="threads", action="store", default="2",
                   help="Number of map-reduce threads per worker")
 parser.add_option("-m", "--modulo", dest="modulo", action="store",
                   help="Remainder when wid is divided by 2; determines odd/even")
