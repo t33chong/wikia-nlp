36039721ca80c1f17ae3fad37575ccaa08652a20
Tristan Euan Chong <tristaneuan@gmail.com>
2013-08-24 03:25:44 +0000

    delete temporary text/filelist files, rewrite xml to gzip

--- nlp-harvester.py
+++ nlp-harvester.py
@@ -4,7 +4,7 @@ Responsible for parsing and writing files in XML format for all content pages on
 Wiki ID is provided as sys.argv[1]
 Number of threads is optionally provided as sys.argv[2]
 """
-import sys, json, gzip, tempfile, requests
+import sys, shutil, json, gzip, tempfile, requests
 from corenlp.corenlp import *
 from WikiaSolr import QueryIterator, get_config, ParserOverseer
 
@@ -31,19 +31,7 @@ def write_text(wid):
     if last_indexed:
         query += ' AND indexed:["%s" TO *]' % last_indexed_value
     qi = QueryIterator(get_config(), {'query': query, 'fields': 'pageid, html_en, indexed', 'sort': 'pageid asc'})
-#    doc_count = 0
-#    batch_count = 0
     for doc in qi:
-#        if doc_count % 100 == 0:
-#            try:
-#                filelist.close()
-#            except NameError:
-#                pass
-#            batch_count += 1
-#            filelist_path = os.path.join(DATA_DIR, 'filelist', str(wid))
-#            if not os.path.exists(filelist_path):
-#                os.makedirs(filelist_path)
-#            filelist = open(os.path.join(filelist_path, 'batch%i' % batch_count), 'w')
         pageid = doc['pageid']
         text = doc.get('html_%s' % language, '').encode('utf-8')
         last_indexed_value = max(last_indexed_value, doc.get('indexed'))
@@ -55,10 +43,9 @@ def write_text(wid):
         text_file = gzip.GzipFile(text_filepath, 'w')
         text_file.write(text)
         text_file.close()
-#        filelist.write(text_filepath + '\n')
-#        doc_count += 1
     with open(os.path.join(DATA_DIR, 'last_indexed.txt'), 'w') as last_indexed_file:
         last_indexed_file.write(last_indexed_value)
+    return text_dir
 
 def write_filelists(wid):
     subdirectories = []
@@ -67,24 +54,32 @@ def write_filelists(wid):
         text_subdir = os.path.join(text_dir, subdir_num)
         files = [os.path.join(text_subdir, f) for f in os.listdir(text_subdir)]
         filelist_dir = os.path.join(DATA_DIR, 'filelist', str(wid))
+        if not os.path.exists(filelist_dir):
+            os.makedirs(filelist_dir)
         filelist_file = os.path.join(filelist_dir, str(subdir_num))
         with open(filelist_file, 'w') as filelist:
             filelist.write('\n'.join(files))
         output_directory = os.path.join(DATA_DIR, 'xml', str(wid), subdir_num)
         subdirectories.append({'index': '%s_%s' % (str(wid), subdir_num), 'command': init_corenlp_command(CORENLP_PATH, MEMORY, PROPERTIES), 'filelist': filelist_file, 'outputDirectory': output_directory})
-    return subdirectories
+    return filelist_dir, subdirectories
 
-#TODO
-"""
-- implement batch parser as part of this script, not in stanford-corenlp-python, to read text .gz files and write to xml .gz files
-- use multiprocess logic in overseer.py for above task
-- delete temporary text .gz and filelist files
-"""
+def convert_xml_to_gzip(subdirectories):
+    for subdir in subdirectories:
+        for xml_filename in os.listdir(subdir['outputDirectory']):
+            xml_filepath = os.path.join(subdir['outputDirectory'], xml_filename)
+            gzip_filepath = xml_filepath + '.gz'
+            gzip_file = gzip.GzipFile(gzip_filepath, 'w')
+            gzip_file.write(open(xml_filepath).read())
+            gzip_file.close()
+            os.remove(xml_filepath)
 
 def main():
-    write_text(wid)
-    subdirectories = write_filelists(wid)
+    text_dir = write_text(wid)
+    filelist_dir, subdirectories = write_filelists(wid)
     ParserOverseer(subdirectories, threads=2).oversee()
+    shutil.rmtree(text_dir)
+    shutil.rmtree(filelist_dir)
+    convert_xml_to_gzip(subdirectories)
 
 if __name__ == '__main__':
     main()
